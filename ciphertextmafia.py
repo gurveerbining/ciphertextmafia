8d5df87e8c218f0a719b958d14850b034d5ca85a
Gurveer Bining <gurveerbining@cmail.carleton.ca>
2025-11-15 15:36:39 -0500

    Initial commit: CipherEnv base

---
+++ ciphertextmafia.py
@@ -0,0 +1,57 @@
+import os, random, string
+import nltk
+from nltk.corpus import brown
+# make sure corpus is downloaded once
+try:
+    nltk.data.find("corpora/brown")
+except LookupError:
+    nltk.download("brown")
+
+class CipherEnv:
+    def __init__(self, text_len=80, max_rounds=10):
+        self.text_len = text_len
+        self.max_rounds = max_rounds
+        self.alphabet = string.ascii_uppercase + " "
+        self.reset()
+
+    def _random_sentence(self):
+        """Generate an English-like plaintext using Brown corpus words."""
+        # choose random start index in the corpus
+        words = [w.upper() for w in brown.words() if w.isalpha()]
+        idx = random.randint(0, len(words) - 30)
+        s = " ".join(words[idx : idx + 30])  # 30 words is plenty
+        s = s.replace("\n", " ")
+        # trim/pad to fixed length
+        s = s[: self.text_len]
+        if len(s) < self.text_len:
+            s = s.ljust(self.text_len)
+        return s
+    
+    def reset(self):
+        # 1) make two plaintexts
+        self.p1 = self._random_sentence()
+        self.p2 = self._random_sentence()
+
+        # 2) make shared one-time-pad key
+        self.key = os.urandom(self.text_len)
+
+        # 3) encrypt both with same key
+        self.c1 = bytes([ord(ch) ^ k for ch, k in zip(self.p1, self.key)])
+        self.c2 = bytes([ord(ch) ^ k for ch, k in zip(self.p2, self.key)])
+
+        # 4) what agents actually see
+        self.x = bytes([a ^ b for a, b in zip(self.c1, self.c2)])
+
+        # 5) initialize masks
+        self.mask1 = ["_"] * self.text_len
+        self.mask2 = ["_"] * self.text_len
+        self.round = 0
+        return self.x
+
+env = CipherEnv()
+
+sentence = env._random_sentence()
+
+print(sentence)
+print(len(sentence))
+
